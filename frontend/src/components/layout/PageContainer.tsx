1cd84e319cf40c7f273d6d87939dea8e3abfcf57
Emirhan Sergen ÇERGEL <117258974+EmirhanSergen@users.noreply.github.com>
2025-06-19 20:35:11 +0300

    Show sidebar only during application

--- frontend/src/components/layout/PageContainer.tsx
+++ frontend/src/components/layout/PageContainer.tsx
@@ -1,14 +1,17 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
 export default function PageContainer() {
+  const { pathname } = useLocation();
+  const showSidebar = pathname.includes("/apply");
+
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
       <div className="flex flex-1">
-        <Sidebar />
-        <main className="flex-1 p-4">
+        {showSidebar && <Sidebar />}
+        <main className={`${showSidebar ? "flex-1" : "w-full"} p-4`}>
           <Outlet />
         </main>
       </div>
