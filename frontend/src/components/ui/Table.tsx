bcc4859947c41d1345eb9321ad31ff5b93d04de4
Emirhan Sergen ÇERGEL <117258974+EmirhanSergen@users.noreply.github.com>
2025-06-20 12:33:12 +0300

    Enhance issues tables with state and alignment

--- frontend/src/components/ui/Table.tsx
+++ frontend/src/components/ui/Table.tsx
@@ -1,8 +1,17 @@
 import { ReactNode } from "react";
+import { cn } from "../../lib/utils";
+interface Props {
+  children: ReactNode;
+  className?: string;
+}
 
-export default function Table({ children }: { children: ReactNode }) {
+export default function Table({ children, className }: Props) {
   return (
-    <table className="min-w-full border" cellPadding={8} cellSpacing={0}>
+    <table
+      className={cn("min-w-full border", className)}
+      cellPadding={8}
+      cellSpacing={0}
+    >
       {children}
     </table>
   );
