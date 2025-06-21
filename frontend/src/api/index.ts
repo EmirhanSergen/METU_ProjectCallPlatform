71e227dbac84aa745e81791243d4d6d06762f5bf
Emirhan Sergen ÇERGEL <117258974+EmirhanSergen@users.noreply.github.com>
2025-06-21 22:26:37 +0300

    fix: export API methods individually

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,7 +1,56 @@
-export * from './auth';
-export * from './calls';
-export * from './applications';
-export * from './applicationForms';
-export * from './mobilityEntries';
-export * from './reviews';
-export * from './users';
+export {
+  login,
+  register,
+  requestPasswordReset,
+} from './auth';
+
+export {
+  getCalls,
+  getCall,
+  createCall,
+  updateCall,
+  deleteCall,
+} from './calls';
+
+export {
+  createApplication,
+  uploadAttachment,
+  uploadProposal,
+  uploadCV,
+  getApplications,
+  getApplicationsByCall,
+  getApplication,
+  getMyApplications,
+  updateApplication,
+  patchApplication,
+  getApplicationAttachments,
+  deleteAttachment,
+  confirmAttachment,
+} from './applications';
+
+export {
+  createApplicationForm,
+  getApplicationForm,
+  updateApplicationForm,
+} from './applicationForms';
+
+export {
+  getMobilityEntries,
+  createMobilityEntry,
+  updateMobilityEntry,
+  deleteMobilityEntry,
+} from './mobilityEntries';
+
+export {
+  submitReview,
+  getReviewReport,
+  getReviewReports,
+  createReviewReport,
+} from './reviews';
+
+export {
+  createUser,
+  updateUser,
+  getUser,
+  listUsers,
+} from './users';
